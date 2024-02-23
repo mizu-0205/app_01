a41ba01f902380a026ed4df201c9fe83f92e48bb
mizu-0205 <mizubin55@docomo.ne.jp>
2024-02-23 12:48:07 +0900

    途中経過

--- microblog/blog/models.py
+++ microblog/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class POST(models.Model):
+    title = models.CharField(max_lengs=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body =  models.TextField()
+    posted_data = models.DataTimeField(auto_now_add=True)
